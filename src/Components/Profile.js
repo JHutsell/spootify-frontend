07f6b0451a814a2c3be587e0452bff27987ada05
esmeryjose <esmerycornielle@gmail.com>
2018-05-04 23:52:58 -0400

    finished user profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Profile = () =>{
-
+const Profile = ({currentUser}) =>{
+    const {profile_img_url, display_name, spotify_url} = currentUser
    return(
        <div>
-           Here
+           <h1>{display_name}</h1>
+           <a href={spotify_url}>
+            <img src={profile_img_url} alt="spotifyImg"/>
+           </a>
        </div>
    )
 }
