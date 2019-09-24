d2e083551a23e840ab8746d03635819f28c1e4d8
Justin Hutsell <hutselljustin@gmail.com>
2019-09-24 17:53:23 -0400

    attempts to fix spotify routing

--- src/Components/App.js
+++ src/Components/App.js
@@ -121,7 +121,8 @@ class App extends Component {
         <SpottyModal searchResults={ this.state.searchResults } showModal={ this.state.showModal } onClose={ this.handleCloseModal }/>
 
       <Switch>
-        <Route path="/callback?code" component={this.handleCallback} />
+        <Route path="/:access_token(access_token=.*)" component={Callback} /> 
+        <Route path="/callback" component={this.handleCallback} />
         {/* <Route exact path='/callback' render={(routerProps) => <Callback currentUser = {this.state.currentUser} {...routerProps} />} /> */}
         <Route exact path='/home' render={(routerProps) => <Home currentUser = {this.state.currentUser} {...routerProps} />} />
         <Route exact path='/profile' render={(routerProps) => <Profile currentUser = {this.state.currentUser} {...routerProps} />} />
